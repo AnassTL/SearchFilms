d416cddef1271212ea452ccfe8175e97babd5411
AnassTL <anasstlemsani99@gmail.com>
2023-06-14 19:29:18 +0100

    Search films

---
+++ exam_anwar/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ListMoviesComponent} from "./list-movies/list-movies.component";
+
+const routes: Routes = [{path:'', component:ListMoviesComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
